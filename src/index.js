4748db2e70d95119a1808956dbd9e9dae066f9d0
Black Chaos <black.chaos.no1@gmail.com>
2023-08-14 18:51:40 +0300

    task 2.1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { Loading } from 'notiflix/build/notiflix-loading-aio';
 import CatAPIService from './cat-api/cat-api';
 import SlimSelect from 'slim-select';
@@ -5,49 +6,62 @@ import SlimSelect from 'slim-select';
 const CatAPI = new CatAPIService();
 
 const catInfo = document.querySelector('.cat-info');
+const select = document.querySelector('.breed-select');
 
-
-createSelectOptions().then(options => {
+createSelectOptions()
+.then(options => {
+    select.classList.remove('is-hidden')
     new SlimSelect({
-      select: document.querySelector('.breed-select'),
+      select: select,
       data: options,
       settings: {
         placeholderText: 'Choose one',
-        },
-        events: {
-          afterChange: onChange
-      }
+        hideSelected: true,
+      },
+      events: {
+        afterChange: onChange,
+      },
     });
-})
-
-function createSelectOptions() {
-    return CatAPI.fetchBreeds()
-      .then(data =>
-        data.map(({ id, name }) => {
-          const option = document.createElement('option');
-          option.value = id;
-          option.textContent = name;
-          return option;
-        })
-      );
+  }).catch(() =>
+  Notify.failure('Oops! Something went wrong! Try reloading the page!')
+  );
+  
+  function createSelectOptions() {
+  Loading.circle();
+  return CatAPI.fetchBreeds().then(data =>
+    data
+      .map(({ id, name }) => {
+        const option = document.createElement('option');
+        option.value = id;
+        option.textContent = name;
+        return option;
+      })
+  ).finally(Loading.remove)
 }
 
 function onChange(select) {
-    Loading.circle();
-    CatAPI.fetchCatByBreed(select[0].value)
-        .then(data => {
-            renderCatInfo({
-                img: data[0].url,
-                name: data[0].breeds[0].name,
-                desc: data[0].breeds[0].description,
-                temp: data[0].breeds[0].temperament,
-            })
-      })
-      .finally(Loading.remove);
+  Loading.circle();
+  CatAPI.fetchCatByBreed(select[0].value)
+  .then(data => {
+    renderCatInfo({
+        img: data[0].url,
+        name: data[0].breeds[0].name,
+        desc: data[0].breeds[0].description,
+        temp: data[0].breeds[0].temperament,
+      });
+    })
+    .catch(() =>{
+      catInfo.innerHTML = '';
+      Notify.failure('Oops! Something went wrong! Try reloading the page!')
+    }
+    )
+    .finally(Loading.remove);
 }
 
-function renderCatInfo({img, name, desc, temp}) {
-    catInfo.innerHTML = `<img src="${img}" alt="cat" width="600"><div class="text-wrap">
+function renderCatInfo({ img, name, desc, temp }) {
+  catInfo.innerHTML =
+    `<img src="${img}" alt="cat" width="600">
+    <div class="text-wrap">
         <h2>${name}</h2>
         <p>${desc}</p>
         <p>${temp}</p>
